c1b808bc7ea0106f2457fb7b868c91b8823efea9
QuangAnh321 <littledragon1998@gmail.com>
2023-07-14 19:29:31 +0700

    fix css not loading

--- src/main/java/com/simple/blog/WebSecurityConfig.java
+++ src/main/java/com/simple/blog/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,7 +16,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(requests -> requests
-            .antMatchers("/").permitAll()
+            .antMatchers( "/", "/register", "/css/**", "/images/**", "/js/**", "/webjars/**").permitAll()
             .anyRequest().authenticated()
         )
         .formLogin(form -> form
@@ -30,5 +31,10 @@ public class WebSecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers( "/css/**", "/images/**", "/js/**", "/webjars/**");
     }
 }
